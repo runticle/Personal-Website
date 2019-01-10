5317a190ab9e3525cd2e0f012e7335038426e330
Alfie Freeman <runticle@gmail.com>
2019-01-10 13:56:18 +0000

    adds links to navbar to blog and homepage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
 
 class NavBar extends Component {
 
   render() {
     return (
-      <div></div>
+      <div id='navigation-bar'>
+        <NavLink to="/blog">Blog</NavLink>
+        <NavLink to="/">Home</NavLink>
+      </div>
     );
   }
 
