d656415e3788a2bf6da90bf48011a0b616b12472
Alfie Freeman <runticle@gmail.com>
2019-01-17 15:32:19 +0000

    adds more about me on homepage

--- src/components/homepage/About.js
+++ src/components/homepage/About.js
@@ -9,6 +9,9 @@ class About extends Component {
     }
     return (
       <div className="about">
+        <div className="my-life">
+          I am Alfie. I like coding and stuff and writing super long winded random text to test my websites and stuff. I love Bitcoin and Blockchain and other amazing Crypto's. I play <a href="https://www.youtube.com/watch?v=lnNSU4kbVvI&t=61s" target="_block">Korfball</a>. I love music.  I like reading stuff. I like travelling places. 
+        </div>
         <h5>STUFF ABOUT ME</h5>
         <img className="prof-pic" src="/assets/smiley.jpg"/>
         <ul className="list-group about-list">
