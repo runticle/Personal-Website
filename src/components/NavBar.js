1241dc4ce08eb638d58b1fe0d51c91a91766758c
Alfie Freeman <runticle@gmail.com>
2019-01-17 20:38:29 +0000

    adds github link to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,7 +16,10 @@ class NavBar extends Component {
         </ul>
         <span className='navbar-text col'> MEMOIRS OF A FREEMAN</span>
         <a href="https://www.linkedin.com/in/alfie-freeman-808166b9/" target="_block">
-          <img src="/assets/In-2c-54px-TM.png" style={{width:'37px',margin:'5px'}}/>
+          <img src="/assets/In-2c-54px-TM.png" style={{width:'34px',marginRight:'5px'}}/>
+        </a>
+        <a href="https://www.github.com/runticle" target="_block">
+          <img src="/assets/GitHub-Mark-64px.png" style={{width:'30px'}}/>
         </a>
       </div>
     );
