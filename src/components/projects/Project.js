b3dfea3d9c07aa70550f4b63247f0a1eaeba93b8
Alfie Freeman <runticle@gmail.com>
2019-01-17 14:26:01 +0000

    updates projects page

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -4,8 +4,9 @@ class Project extends Component {
 
   render() {
     return (
-      <div>
-        <a href={this.props.link}>{this.props.name}</a>
+      <div className="project-box col-sm container">
+        <a href={this.props.link} target="_blank">{this.props.name}</a>
+        <p>{this.props.description}</p>
       </div>
     );
   }
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import NavBar from '../NavBar'
 import Project from './Project'
+import MakersRich from './MakersRich'
 
 class Projects extends Component {
 
@@ -13,7 +14,7 @@ class Projects extends Component {
   }
 
   componentDidMount() {
-    fetch("https://api.github.com/users/runticle/repos")
+    fetch("https://api.github.com/users/runticle/repos?sort=created")
       .then(res => res.json())
       .then((res) => {
         this.getProjectsArray(res)
@@ -23,7 +24,7 @@ class Projects extends Component {
   getProjectsArray(json){
     let projectArray = []
     json.forEach(function(proj) {
-      projectArray.push({name: proj.name, link: proj.html_url})
+      projectArray.push({name: proj.name, link: proj.html_url, description: proj.description})
     })
     this.setState({
       projects: projectArray
@@ -34,9 +35,16 @@ class Projects extends Component {
     return (
       <div>
         <NavBar/>
-        {this.state.projects.map(function(proj, index) {
-          return <Project key={index} name={proj.name} link={proj.link}/>
-        })}
+        <div id="project-page">
+          <h2>GITHUB PROJECTS</h2>
+          <p id="projects-description" className="col-sm container">
+            Here lies all public project repos I have on github from the very beginning. Yes, most of them are unfinished, and not brilliant to boot. It is a learning curve and hopefully you can see improvments along the way!
+          </p>
+          <MakersRich/>
+          {this.state.projects.map(function(proj, index) {
+            return <Project key={index} name={proj.name} link={proj.link} description={proj.description}/>
+          })}
+        </div>
       </div>
     );
   }
